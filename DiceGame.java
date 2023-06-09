6b14d8ebf0b8f00f5ef4645def6196b9e54265f6
MATSUMOTO Yudai <s1300105@linsv216.u-aizu.ac.jp>
2023-06-09 12:42:35 +0900

    Won/lost message added

--- DiceGame.java
+++ DiceGame.java
@@ -9,9 +9,12 @@ class DiceGame{
 	b = random.nextInt(6)+1;
 	
 	c=a+b;
+	
 	System.out.println("Rolling dice...");
 	System.out.println("Die 1: "+a);
 	System.out.println("Die 2: "+b);
 	System.out.println("Total value: "+c);
+	if(c>=7) System.out.println("You won");
+	else System.out.println("You lost");
     }
 }
